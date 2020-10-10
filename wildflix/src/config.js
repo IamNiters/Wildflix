cf68e399808a808fb9a897c7bea84e151edeefc3
Niters <niters69@gmail.com>
2020-10-10 11:47:14 +0200

    Added Api.js & .env config

---
+++ wildflix/src/config.js
@@ -0,0 +1,17 @@
+// Full documentation at : https://developers.themoviedb.org
+
+
+// Api_key
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+
+// Images_width_display
+
+const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/' ;
+// sizes : w300, w780, w1280, original
+
+const BACKDROP_SIZE = 'w1280';
+// w92, w154, w185, w342, w500, w780, original
+
+const POSTER_SIZE = 'w780';
